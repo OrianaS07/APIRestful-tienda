1b45816d38c68ad3a210c98cddad3f75318776f0
Oriana <ori@oriana.com>
2021-03-25 10:22:18 -0400

    3. Creacion de Routes y eliminacion Se eliminaros Buyer y Seller, y que posteriormente se establecera un sistema de les y permisos

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,19 @@
 <?php
 
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
+use GuzzleHttp\Handler\Proxy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::resource('categories', CategoryController::class);
+
+Route::resource('products', ProductController::class);
+
+Route::resource('transactions', TransactionController::class);
+
+Route::resource('users', UserController::class);
 
 
